18c5ec0b1b72e5b326b3f2b9e3c336035ac0b46e
rachelch <rachelch@I-ROX-175>
2019-01-29 12:06:21 +0200

    added folders

---
+++ console/index.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const smoochBot = require('smooch-bot');
+const MemoryStore = smoochBot.MemoryStore;
+const MemoryLock = smoochBot.MemoryLock;
+const Bot = smoochBot.Bot;
+const Script = smoochBot.Script;
+const StateMachine = smoochBot.StateMachine;
+
+const jwt = require('jsonwebtoken');
+
+var token = jwt.sign({scope: 'app'}, 'yALmS9bSdH9IZf5Hw4TArSTt54k_IqXKrivTraNxbZQPagGoKrhqq5hCpAPY-Md8OXpMnUhB8EzHXEG0blzoBw', {header: {kid: 'app_5c4d91c9c74c040023423c98'}});
+console.log(token);
+
+
+class ConsoleBot extends Bot {
+    constructor(options) {
+        super(options);
+    }
+
+    say(text) {
+        return new Promise((resolve) => {
+            console.log(text);
+            resolve();
+        });
+    }
+}
+
+const script = new Script({
+    start: {
+        receive: (bot) => {
+            return bot.say('Hi! I\'m Smooch Bot!')
+                .then(() => 'askName');
+        }
+    },
+
+    askName: {
+        prompt: (bot) => bot.say('What\'s your name'),
+        receive: (bot, message) => {
+            const name = message.text.trim();
+            bot.setProp('name', name);
+            return bot.say(`I'll call you ${name}! Great!`)
+                .then(() => 'finish');
+        }
+    },
+
+    finish: {
+        receive: (bot, message) => {
+            return bot.getProp('name')
+                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
+                        'teach me how to do anything else!'))
+                .then(() => 'finish');
+        }
+    }
+});
+
+const userId = 'testUserId';
+const store = new MemoryStore();
+const lock = new MemoryLock();
+const bot = new ConsoleBot({
+    store,
+    lock,
+    userId
+});
+
+const stateMachine = new StateMachine({
+    script,
+    bot,
+    userId
+});
+
+process.stdin.on('data', function(data) {
+    stateMachine.receiveMessage({
+        text: data.toString().trim()
+    })
+        .catch((err) => {
+            console.error(err);
+            console.error(err.stack);
+        });
+});
