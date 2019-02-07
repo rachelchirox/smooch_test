408b0f0cc41a47d1ff9603b1891f099a5926412e
rachelch <rachelch@I-ROX-175>
2019-02-07 15:04:22 +0200

    fix

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // app.get('/', function(req, res) {
