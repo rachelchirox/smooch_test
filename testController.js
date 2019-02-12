834c091c1a0dcd7e32ab25395172ed406165c891
rachelch <rachelch@i-rox.net>
2019-02-12 12:25:36 +0200

    fix

--- testController.js
+++ testController.js
@@ -8,7 +8,7 @@ router.route('/sendMessageToClient')
         console.log('sendMessageToClient body: ' + JSON.stringify(req.body, null, 4));
         let userId = req.body.sessionId;
         //let clientType = req.body.clientType;//not supposed to send to this layer, remove it later.
-        let dataObject = JSON.parse(req.body.content);
+        let dataObject = req.body.content;
         messagesManager.handleReponseFromServer(dataObject, userId);
         res.json('ok');
     });
