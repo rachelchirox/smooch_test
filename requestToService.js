14a447c12d637ff759cd56138b0f4e5a7c6670c4
rachelch <rachelch@i-rox.net>
2019-02-28 14:26:59 +0200

    fix

--- requestToService.js
+++ requestToService.js
@@ -1,12 +1,26 @@
 "use strict";
 const httpServiceLocator = require('./httpServiceLocator');
 
-let requestToService = function () {
-};
+let requestToService = {
 
-requestToService.prototype = {
+
+    isInProcess : false,
+
+    sendWrapper : function(url, method, body = {}) {
+
+        let time = 0;
+        if (requestToService.isInProcess){
+            time = 2000;
+        }
+        setTimeout(function() {
+            requestToService.sendRequest(url, method, body);
+            console.log('Blah blah blah blah extra-blah');
+        }, time);
+    },
+
     sendRequest: function (url, method, body = {}) {
         return new Promise((resolve, reject) => {
+            requestToService.isInProcess = true;
             console.log('url: ' + url);
             console.log('method: ' + method);
             console.log('body: ' + JSON.stringify(body));
@@ -14,19 +28,21 @@ requestToService.prototype = {
                 (response) => {
                     console.log('111*');
                     console.log('response: ' + JSON.stringify(response), 'yellow');
+                    requestToService.isInProcess = false;
                     resolve(response);
                 },
                 (error) => {
                     console.log('222*');
                     console.error(error);
+                    requestToService.isInProcess = false;
                     reject(error);
                 }).catch((ex) => {
                 console.log('333*');
+                requestToService.isInProcess = false;
                 reject(ex);
             })
         })
     }
 };
 
-let instance = new requestToService();
-module.exports = instance;
+module.exports = requestToService;
--- testController.js
+++ testController.js
@@ -14,19 +14,19 @@ router.route('/sendMessageToClient')
         let dataObject = req.body.content;
         //messagesManager.handleResponseFromServer(dataObject, userId);
 
-        // requestToService.sendRequest(exteranl_service_path , 'post', req.body).then(data => {
-        //     let dataObject = JSON.parse(data);
-        //     console.log('response from ' + exteranl_service_path + ' :' + JSON.stringify(dataObject));
-        // });
+        requestToService.sendWrapper(exteranl_service_path , 'post', req.body).then(data => {
+            let dataObject = JSON.parse(data);
+            console.log('response from ' + exteranl_service_path + ' :' + JSON.stringify(dataObject));
+        });
 
 
-        setTimeout(function() {
-            requestToService.sendRequest(exteranl_service_path , 'post', req.body).then(data => {
-                let dataObject = JSON.parse(data);
-                console.log('response from ' + exteranl_service_path + ' :' + JSON.stringify(dataObject));
-            });
-            console.log('Blah blah blah blah extra-blah');
-        }, 3000);
+        // setTimeout(function() {
+        //     requestToService.sendRequest(exteranl_service_path , 'post', req.body).then(data => {
+        //         let dataObject = JSON.parse(data);
+        //         console.log('response from ' + exteranl_service_path + ' :' + JSON.stringify(dataObject));
+        //     });
+        //     console.log('Blah blah blah blah extra-blah');
+        // }, 3000);
 
 
         res.json('ok');
